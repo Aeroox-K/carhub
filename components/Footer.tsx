16aae3002f4eacb58163041224fe14ee75bc7ad7
Aeroox <kayroog1@gmail.com>
2024-10-10 00:59:41 +0100

    fixed-footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,12 +24,35 @@ const Footer = () => {
                 </p>
             </div>
 
-                <div className="footer__links" >
+            <div className="flex-[1.5] w-full flex flex-row md:justify-end md:gap-36 gap-16 flex-wrap md:mt-0 mt-5" >
+            {footerLinks.map((link) => (
+
+                <div key={link.title} className="flex flex-col min-w-[100px] md:mt-0 mt-6" >
+                  <h3 className="font-bold" >
+                  {link.title}
+                  </h3> 
+
+                  <ul className="" >
+                      {link.links.map((item) => (
+                        <Link
+                           key={item.title}
+                           href={item.url}
+                           className="text-gray-500">
+                          <li className="mt-2">
+                            {item.title}
+                          </li>
+                        </Link>
+                    ))}
+                  </ul>
+                </div>
+            ))}
+            </div>
+
+                {/* <div className="footer__links" >
                   {footerLinks.map((link) => (
 
                     <div key={link.title} 
                     className="footer__link">
-                      
                       <h3 className="font-bold" >
                         {link.title}
                       </h3>
@@ -40,20 +63,19 @@ const Footer = () => {
                           href={item.url}
                           className="text-gray-500"
                         >
-
                           {item.title}
-
                       </Link>                          
                         ))}
                       
                     </div>
                   ))}
-                </div>
+                </div> */}
 
             </div>
 
-                <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 flex-col sm:flex-row sm:px-10 px-24 py-10">
+                <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 md:flex-row flex-col  sm:px-10 md:px-24 px:2 py-10">
                   <p>@2024 Carhub. All Rights Reserved</p>
+
                   <div className="footer__copyrights-link" >
                     <Link href="/" 
                     className="text-gray-500" >
@@ -64,6 +86,7 @@ const Footer = () => {
                       Terms of Use
                     </Link>
                   </div>
+
                 </div>
         
     </footer>
